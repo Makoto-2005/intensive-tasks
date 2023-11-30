9f7a9345f0ca45e06c32cc44fb714282994cee57
Makoto-2005 <makoto2005@yandex.ru>
2023-11-30 20:41:21 +0300

    correct the multiple constructions of "if"

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -26,31 +26,28 @@ public class Task4 {
      * Количество решений: 0.
      */
     static String solveQuadraticEquation(double a, double b, double c) {
-        if (a == 0) {
-            if (b == 0) {
-                if (c == 0) {
-                    return "Решений бесконечно";
-                }
-                return "Количество решений: 0.";
+        if (a != 0) {
+            double discriminant = Math.pow(b, 2) - 4 * a * c;
+
+            if (discriminant > 0) {
+                double firstRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
+                double secondRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+                return "Количество решений: 2. Корни: " + firstRoot + ";" + secondRoot;
             }
+
+            if (discriminant == 0) {
+                double root = -b / (2 * a);
+                return "Количество решений: 1. Корень: " + root;
+            }
+
+            return "Количество решений: 0.";
+        }
+        if (b != 0) {
             double root = -c / b;
             return "Количество решений: 1. Корень: " + root;
         }
+        return c == 0 ? "Решений бесконечно" : "Количество решений: 0.";
 
-        double discriminant = Math.pow(b, 2) - 4 * a * c;
-
-        if (discriminant > 0) {
-            double firstRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
-            double secondRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
-            return "Количество решений: 2. Корни: " + firstRoot + ";" + secondRoot;
-        }
-
-        if (discriminant == 0) {
-            double root = -b / (2 * a);
-            return "Количество решений: 1. Корень: " + root;
-        }
-
-        return "Количество решений: 0.";
     }
 }
 
